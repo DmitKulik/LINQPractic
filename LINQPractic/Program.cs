6add4ea0bc86fb5fa04d55be65744082ff47de61
Dmitry <grammattika@gmail.com>
2023-07-02 01:19:01 +0300

    Задача: сделать выборку городов-миллионников по всем странам.

--- LINQPractic/Program.cs
+++ LINQPractic/Program.cs
@@ -12,6 +12,9 @@ namespace LINQPractic
     {
         static void Main(string[] args)
         {
+            // Словарь для хранения стран с городами
+            var Countries = new Dictionary<string, List<City>>();
+
             // Добавим Россию с её городами
             var russianCities = new List<City>();
             russianCities.Add(new City("Москва", 11900000));
@@ -19,9 +22,34 @@ namespace LINQPractic
             russianCities.Add(new City("Волгоград", 1099000));
             russianCities.Add(new City("Казань", 1169000));
             russianCities.Add(new City("Севастополь", 449138));
- 
-            foreach (var city1 in russianCities.Where(f => f.Name.Length <= 10).OrderBy(f => f.Name.Length))
-                     Console.WriteLine(@$"Город " + city1.Name);
+            Countries.Add("Россия", russianCities);
+
+            // Добавим Беларусь
+            var belarusCities = new List<City>();
+            belarusCities.Add(new City("Минск", 1200000));
+            belarusCities.Add(new City("Витебск", 362466));
+            belarusCities.Add(new City("Гродно", 368710));
+            Countries.Add("Беларусь", belarusCities);
+
+            // Добавим США
+            var americanCities = new List<City>();
+            americanCities.Add(new City("Нью-Йорк", 8399000));
+            americanCities.Add(new City("Вашингтон", 705749));
+            americanCities.Add(new City("Альбукерке", 560218));
+            Countries.Add("США", americanCities);
+
+
+
+            // Задача: сделать выборку городов-миллионников по всем странам.
+
+            var cities = from country in Countries // пройдемся по странам
+                         from city in country.Value // пройдемся по городам
+                         where city.Population > 1000000 // выберем города - миллионники
+                         orderby city.Population descending // отсортируем по населению
+                         select city;
+
+            foreach (var city in cities)
+                Console.WriteLine(city.Name + " - " + city.Population);
 
         }
 
